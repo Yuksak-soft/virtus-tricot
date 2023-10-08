e7af611146fb84bdcd72e255bd368a6457790317
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-08 18:25:07 +0500

    add new codes

--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -7,28 +7,77 @@ import { useTranslation } from 'react-i18next';
 
 interface PortfolioProps { }
 
+interface ItemProps {
+    image: string;
+}
+
+const topItems: ItemProps[] = [
+    { image: "/1.jpg" },
+    { image: "/2.jpg" },
+    { image: "/3.jpg" },
+    { image: "/4.jpg" },
+    { image: "/5.jpg" },
+    { image: "/6.jpg" },
+    { image: "/7.jpg" },
+    { image: "/8.jpg" },
+    { image: "/9.jpg" },
+    { image: "/10.jpg" },
+    { image: "/11.jpg" },
+    { image: "/12.jpg" },
+    { image: "/13.jpg" },
+    { image: "/14.jpg" },
+    { image: "/15.jpg" },
+    { image: "/16.jpg" },
+    { image: "/17.jpg" },
+    { image: "/18.jpg" },
+    { image: "/19.jpg" },
+    { image: "/20.jpg" },
+];
+
+const bottomItems: ItemProps[] = [
+    { image: "/21.jpg" },
+    { image: "/22.jpg" },
+    { image: "/23.jpg" },
+    { image: "/24.jpg" },
+    { image: "/25.jpg" },
+    { image: "/26.jpg" },
+    { image: "/27.jpg" },
+    { image: "/28.jpg" },
+    { image: "/29.jpg" },
+    { image: "/30.jpg" },
+    { image: "/31.jpg" },
+    { image: "/32.jpg" },
+    { image: "/33.jpg" },
+    { image: "/34.jpg" },
+    { image: "/35.jpg" },
+    { image: "/36.jpg" },
+    { image: "/37.jpg" },
+    { image: "/38.jpg" },
+    { image: "/39.jpg" },
+]
+
 const Portfolio: React.FC<PortfolioProps> = () => {
 
     const { t } = useTranslation()
 
     const propsSection1 = useSpring({
         loop: true,
-        config: { duration: 20000 },
+        config: { duration: 25000 },
         to: async (next) => {
             while (true) {
-                await next({ transform: 'translateX(-100%)' });
-                await next({ transform: 'translateX(100%)' });
+                await next({ transform: 'translateX(-150%)' });
+                await next({ transform: 'translateX(150%)' });
             }
         },
     });
 
     const propsSection2 = useSpring({
         loop: true,
-        config: { duration: 20000 },
+        config: { duration: 25000 },
         to: async (next) => {
             while (true) {
-                await next({ transform: 'translateX(100%)' });
-                await next({ transform: 'translateX(-100%)' });
+                await next({ transform: 'translateX(150%)' });
+                await next({ transform: 'translateX(-150%)' });
             }
         },
     });
@@ -39,24 +88,20 @@ const Portfolio: React.FC<PortfolioProps> = () => {
         <div className={cls.centerLine}></div>
         <div className={cls.products}>
             <animated.div style={propsSection2} className={cls.section1}>
-                <Image src="/1.svg" alt='item' width={340} height={200} />
-                <div className={cls.line}></div>
-                <Image src="/2.svg" alt='item' width={340} height={200} />
-                <div className={cls.line}></div>
-                <Image src="/3.svg" alt='item' width={340} height={200} />
-                <div className={cls.line}></div>
-                <Image src="/4.svg" alt='item' width={340} height={200} />
-                <div className={cls.line}></div>
+                {topItems.map(item => (
+                    <React.Fragment key={item.image}>
+                        <Image src={item.image} alt='item' width={400} height={200} />
+                        <div className={cls.line}></div>
+                    </React.Fragment>
+                ))}
             </animated.div>
             <animated.div style={propsSection1} className={cls.section2}>
-                <div className={cls.line}></div>
-                <Image src="/5.svg" alt='item' width={340} height={200} />
-                <div className={cls.line}></div>
-                <Image src="/6.svg" alt='item' width={340} height={200} />
-                <div className={cls.line}></div>
-                <Image src="/7.svg" alt='item' width={340} height={200} />
-                <div className={cls.line}></div>
-                <Image src="/8.svg" alt='item' width={340} height={200} />
+                {bottomItems.map(item => (
+                    <React.Fragment key={item.image}>
+                        <Image src={item.image} alt='item' width={400} height={200} />
+                        <div className={cls.line}></div>
+                    </React.Fragment>
+                ))}
             </animated.div>
         </div>
 
