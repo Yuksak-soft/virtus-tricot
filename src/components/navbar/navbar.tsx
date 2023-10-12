3dd87a010ff9cc5f7772023a9fff314038943e5d
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-12 12:16:12 +0500

    add new codes

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -62,7 +62,7 @@ const Navbar: React.FC<NavbarProps> = () => {
                     <Select
                         defaultValue="ru"
                         className={cls.languageSelector}
-                        style={{ width: 65, }}
+                        style={{ width: 75, }}
                         onChange={handleChange}
                         options={[
                             { value: 'uz', label: <span style={{ fontSize: 20 }}>🇺🇿</span> },
